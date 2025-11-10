7d00e49c93c43c7e70c44afaf48dd409785b9d83
TanijRoy1 <tanishroy480@gmail.com>
2025-11-10 19:09:50 +0600

    Get course by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -35,6 +35,12 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/courses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await courseCollection.findOne(query);
+      res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
