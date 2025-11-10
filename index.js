b413f9127c9005fc29677636faa8b1f9fff6502e
TanijRoy1 <tanishroy480@gmail.com>
2025-11-10 19:20:16 +0600

    Get popularCourses filtered by isFeatured=true

--- index.js
+++ index.js
@@ -35,12 +35,19 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/popularCourses", async (req, res) => {
+      const query = {isFeatured : true};
+      const cursor = courseCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     app.get("/courses/:id", async (req, res) => {
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
       const result = await courseCollection.findOne(query);
       res.send(result);
     })
+    
 
     await client.db("admin").command({ ping: 1 });
     console.log(
